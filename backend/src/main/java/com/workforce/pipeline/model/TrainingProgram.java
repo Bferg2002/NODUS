13a4d10c7922140ea2f3e6f2606ddd5940c63ba6
wilfredainsworth <wainsworth261@gmail.com>
2026-04-16 11:41:37 -0400

    Completed Repository connections for these classes. (Job,Role,Skill, & Training)

--- backend/src/main/java/com/workforce/pipeline/model/TrainingProgram.java
+++ backend/src/main/java/com/workforce/pipeline/model/TrainingProgram.java
@@ -1,4 +1,59 @@
 package com.workforce.pipeline.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TrainingProgram {
+    @Getter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Getter
+    private String name;
+    @Getter
+    private String description;
+
+    @ManyToMany
+    @JoinTable(
+            name = "training_program_skill",
+            joinColumns = @JoinColumn(name = "training_program_id"),
+            inverseJoinColumns = @JoinColumn(name = "skill_id")
+    )
+    private List<Skill> skillsTaught;
+
+    public TrainingProgram() {
+        this.skillsTaught = new ArrayList<>();
+    }
+
+    public TrainingProgram(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.skillsTaught = new ArrayList<>();
+    }
+
+    public void addSkill(Skill skill) {
+        skillsTaught.add(skill);
+    }
+
+    public void removeSkill(Skill skill) {
+        skillsTaught.remove(skill);
+    }
+
+    public List<Skill> getSkillsTaught() {
+        return new ArrayList<>(skillsTaught);
+    }
+
 }
